a306c4b82119faac98edb0fb8baf6ab24a498a66
Twan Meurs <tmeurs@student.che.nl>
2025-10-10 14:49:02 +0200

    add in extra nav change

--- scripts/generator.js
+++ scripts/generator.js
@@ -126,23 +126,28 @@ function createBlogHtmlPage(data, markdownContent, marked) {
         <a href="/index.html"><img src="/images/logo.png" alt="Logo Stadspartij Wageningen" /></a>
       </div>
       <nav class="main-nav">
-        <ul>
-          <li><a href="/index.html">Home</a></li>
-          <li><a href="/nieuws.html" class="active">Nieuws</a></li>
-          <li class="dropdown">
-            <a href="#" class="drop-btn">Verkiezingen</a>
-            <div class="dropdown-content">
-              <a href="/samenvatting-verkiezingsprogramma.html">Samenvatting Programma</a>
-              <a href="/verkiezingsprogramma-compleet.html">Volledig Programma</a>
-              <a href="/kieslijst.html">Kieslijst</a>
-              <a href="/stellingen-stemwijzer.html">Stellingen Stemwijzer</a>
-              <a href="/stellingen-mijnstem.html">Stellingen MijnStem</a>
-            </div>
-          </li>
-          <li><a href="/fractie.html">Fractie</a></li>
-          <li><a href="/contact.html">Contact</a></li>
-        </ul>
-      </nav>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="fractie.html">Fractie</a></li>
+            <li><a href="nieuws.html" class="active" >Nieuws</a></li>
+            <li><a href="ambassadeur.html">Word ambassadeur</a></li>
+            <li class="dropdown">
+              <a href="#" class="drop-btn">Verkiezingen</a>
+              <div class="dropdown-content">
+                <a href="samenvatting-verkiezingsprogramma.html"
+                  >Samenvatting Programma</a
+                >
+                <a href="verkiezingsprogramma-compleet.html"
+                  >Volledig Programma</a
+                >
+                <!-- <a href="kieslijst.html">Kieslijst</a> -->
+                <!-- <a href="stellingen-stemwijzer.html">Stellingen Stemwijzer</a> -->
+                <a href="stellingen-mijnstem.html">Stellingen Stemwijzer</a>
+              </div>
+            </li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
     </div>
   </header>
 
@@ -252,23 +257,28 @@ function createRaadslidHtmlPage(data, markdownBioContent, marked) {
         <a href="/index.html"><img src="/images/logo.png" alt="Logo Stadspartij Wageningen" /></a>
       </div>
       <nav class="main-nav">
-        <ul>
-          <li><a href="/index.html">Home</a></li>
-          <li><a href="/nieuws.html">Nieuws</a></li>
-          <li class="dropdown">
-            <a href="#" class="drop-btn">Verkiezingen</a>
-            <div class="dropdown-content">
-              <a href="/samenvatting-verkiezingsprogramma.html">Samenvatting Programma</a>
-              <a href="/verkiezingsprogramma-compleet.html">Volledig Programma</a>
-              <a href="/kieslijst.html">Kieslijst</a>
-              <a href="/stellingen-stemwijzer.html">Stellingen Stemwijzer</a>
-              <a href="/stellingen-mijnstem.html">Stellingen MijnStem</a>
-            </div>
-          </li>
-          <li><a href="/fractie.html" class="active">Fractie</a></li>
-          <li><a href="/contact.html">Contact</a></li>
-        </ul>
-      </nav>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="fractie.html" class="active">Fractie</a></li>
+            <li><a href="nieuws.html">Nieuws</a></li>
+            <li><a href="ambassadeur.html">Word ambassadeur</a></li>
+            <li class="dropdown">
+              <a href="#" class="drop-btn">Verkiezingen</a>
+              <div class="dropdown-content">
+                <a href="samenvatting-verkiezingsprogramma.html"
+                  >Samenvatting Programma</a
+                >
+                <a href="verkiezingsprogramma-compleet.html"
+                  >Volledig Programma</a
+                >
+                <!-- <a href="kieslijst.html">Kieslijst</a> -->
+                <!-- <a href="stellingen-stemwijzer.html">Stellingen Stemwijzer</a> -->
+                <a href="stellingen-mijnstem.html">Stellingen Stemwijzer</a>
+              </div>
+            </li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
     </div>
   </header>
 
