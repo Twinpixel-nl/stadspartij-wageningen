9e98cc1725a4aad94856dd548ecad0b0a08e62ee
Twan Meurs <twanmeurs10@gmail.com>
2026-01-02 15:10:59 +0100

    html mee in gen

--- scripts/generator.js
+++ scripts/generator.js
@@ -483,7 +483,7 @@ function copyStaticFiles() {
     "contact.html", "fractie.html", "index.html", "kieslijst.html", "nieuws.html",
     "samenvatting-verkiezingsprogramma.html", "stellingen-mijnstem.html",
     "stellingen-stemwijzer.html", "verkiezingsprogramma-compleet.html",
-    "verkiezingsprogramma.html", "ambassadeur.html", "robots.txt", "sitemap.xml"
+    "verkiezingsprogramma.html", "ambassadeur.html", "robots.txt", "sitemap.xml", "verkiezingsprogramma-engels.html"
   ];
   const dirsToCopy = ["css", "js", "images"];
 
